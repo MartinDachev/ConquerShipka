b4dc109389ddcd3caa4f837a78d433ea6a785f7f
MartinDachev <martin.dachev@hotmail.com>
2015-05-21 13:36:07 +0300

    Added placeholder mesh

---
+++ Conquer Shipka/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+
+    private const string typeName = "UniqueGameName";
+    private const string gameName = "RoomName";
+    public GameObject playerPrefab;
+
+    void Start()
+    {
+
+    }
+
+    private void StartServer()
+    {
+        Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(typeName, gameName);
+    }
+
+    void OnServerInitialized()
+    {
+        //Debug.Log("Server Initializied: " + MasterServer.ipAddress + MasterServer.port);
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        Network.Instantiate(playerPrefab, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+    }
+
+    private void JoinServer(HostData hostData)
+    {
+        Network.Connect(hostData);
+    }
+
+    void OnConnectedToServer()
+    {
+        //Debug.Log("Server Joined");
+        SpawnPlayer();
+    }
+
+    void OnGUI()
+    {
+        if (!Network.isClient && !Network.isServer)
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                StartServer();
+
+            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+                RefreshHostList();
+
+            if (hostList != null)
+            {
+                for (int i = 0; i < hostList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+                        JoinServer(hostList[i]);
+                }
+            }
+        }
+    }
+
+    private HostData[] hostList;
+
+    private void RefreshHostList()
+    {
+        MasterServer.RequestHostList(typeName);
+    }
+
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        if (msEvent == MasterServerEvent.HostListReceived)
+            hostList = MasterServer.PollHostList();
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
---
+++ Conquer Shipka/Assets/Scripts/TakeDamage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeDamage : MonoBehaviour 
+{
+	public float health;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void Damage(float damage)
+	{
+		health = health - damage;
+		if (health <= 0) 
+		{
+			Destroy (this.gameObject);
+		}
+	}
+}
