4f8ace4cc40537fbeaaa3062ee58e76b03a87a7f
MartinDachev <martin.dachev@hotmail.com>
2016-07-06 23:48:58 +0300

    Big Update - Added networking, improved everything else
    Added multiplayer, position syncing and smoothing, rotation syncing and
    smoothing, improved bullets, movement and fixed bugs

--- Conquer Shipka/Assets/Scripts/NetworkManager.cs
+++ Conquer Shipka/Assets/Scripts/NetworkManager.cs
@@ -1,84 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkManager : MonoBehaviour {
-
-	// Use this for initialization
-
-    private const string typeName = "UniqueGameName";
-    private const string gameName = "RoomName";
-    public GameObject playerPrefab;
-
+public class NetworkManager : MonoBehaviour 
+{
+    public Camera standbyCamera;
+    GameObject myPlayerGO;
     void Start()
     {
-
+        Connect();
     }
 
-    private void StartServer()
+    void Connect()
     {
-        Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
-        MasterServer.RegisterHost(typeName, gameName);
-    }
-
-    void OnServerInitialized()
-    {
-        //Debug.Log("Server Initializied: " + MasterServer.ipAddress + MasterServer.port);
-        SpawnPlayer();
-    }
-
-    private void SpawnPlayer()
-    {
-        Network.Instantiate(playerPrefab, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
-    }
-
-    private void JoinServer(HostData hostData)
-    {
-        Network.Connect(hostData);
-    }
-
-    void OnConnectedToServer()
-    {
-        //Debug.Log("Server Joined");
-        SpawnPlayer();
+        PhotonNetwork.ConnectUsingSettings("0.0.0.1");
     }
 
     void OnGUI()
     {
-        if (!Network.isClient && !Network.isServer)
-        {
-            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
-                StartServer();
-
-            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
-                RefreshHostList();
-
-            if (hostList != null)
-            {
-                for (int i = 0; i < hostList.Length; i++)
-                {
-                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
-                        JoinServer(hostList[i]);
-                }
-            }
-        }
+		//GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
     }
 
-    private HostData[] hostList;
-
-    private void RefreshHostList()
+    void OnJoinedLobby()
     {
-        MasterServer.RequestHostList(typeName);
+        Debug.Log("OnJoinedLobby");
+        PhotonNetwork.JoinRandomRoom();
     }
 
-    void OnMasterServerEvent(MasterServerEvent msEvent)
+    void OnPhotonRandomJoinFailed()
     {
-        if (msEvent == MasterServerEvent.HostListReceived)
-            hostList = MasterServer.PollHostList();
+        Debug.Log("OnPhotonJoinFailed");
+        PhotonNetwork.CreateRoom(null);
     }
 
-	// Update is called once per frame
-	void Update () 
+    void OnJoinedRoom()
     {
-	
-	}
+        Debug.Log("OnJoinedRoom");
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        myPlayerGO = PhotonNetwork.Instantiate("PlayerController", new Vector3(10,10,10), Quaternion.identity, 0);
+        standbyCamera.enabled = false;
+		myPlayerGO.SetActive (true);
+        ((MonoBehaviour)myPlayerGO.GetComponent(typeof(PlayerMovement))).enabled = true;
+        Camera cam = ((Camera)myPlayerGO.GetComponentInChildren(typeof(Camera)));
+        cam.enabled = true;
+		((MonoBehaviour)myPlayerGO.GetComponentInChildren(typeof(GunMovement))).enabled = true;
+		((MonoBehaviour)myPlayerGO.GetComponentInChildren(typeof(GunShooting))).enabled = true;
+		((MonoBehaviour)myPlayerGO.GetComponentInChildren(typeof(GunProperties))).enabled = true;
+		((MonoBehaviour)myPlayerGO.GetComponentInChildren(typeof(BulletManager))).enabled = true;
+		Transform partSysLocal = (Transform)myPlayerGO.transform.Find ("Main Camera/BulletsPartSystemLocal");
+		if (partSysLocal == null) {
+			Debug.Log ("partSysLocal is null");
+		}
+		partSysLocal.gameObject.SetActive (true);
+		Transform partSysAll = (Transform)myPlayerGO.transform.Find ("Main Camera/BulletsPartSystemAll");
+		if (partSysAll == null) {
+			Debug.Log ("partSysAll is null");
+		}
+		partSysAll.gameObject.SetActive (false);
+    }
 }
